450fe2ea5dd1ccf5a703659c29604c7635fb2783
SalVN <sal.v.newell@gmail.com>
2017-06-02 14:22:20 +0100

    [last] returns the last n values of an array or string

--- main.js
+++ main.js
@@ -13,6 +13,15 @@ _.first = (array, n) => {
     return result;
 };
 
+//  LAST
+_.last = (array, n) => {
+    if (!(typeof array === 'string') && !(Array.isArray(array))) return undefined;
+    if (n === undefined) return array[array.length - 1];
+    let result = array.slice(array.length - n);
+    if (typeof result === 'string') result = result.split('');
+    return result;
+};
+
 
 
 if (typeof module !== 'undefined') {
--- spec/main.spec.js
+++ spec/main.spec.js
@@ -177,6 +177,129 @@ describe('_', function () {
         });
     });
 
+    //  LAST
+    describe('_.last', function () {
+        it('is a function', function () {
+            expect(_.last).to.be.a('function');
+        });
+
+        it('takes two arguments', function () {
+            expect(_.last).to.have.lengthOf(2);
+        });
+
+        it('should return undefined for an empty array/string or if a non-array/string last argument is given', function () {
+            const result1 = _.last();
+            expect(result1).to.be.undefined;
+            
+            const result2 = _.last([]);
+            expect(result2).to.be.undefined;
+
+            const result3 = _.last('');
+            expect(result3).to.be.undefined;
+
+            const result4 = _.last({a: 1, b: 2});
+            expect(result4).to.be.undefined;
+
+            const result5 = _.last(true);
+            expect(result5).to.be.undefined;
+
+            const result6 = _.last(12345);
+            expect(result6).to.be.undefined;
+        });
+
+        it('should return the last element of an array if no second argument is provided', function () {
+            const result1 = _.last([1,2,3,4,5]);
+            expect(result1).to.equal(5);
+
+            const result2 = _.last(['hello', 'world']);
+            expect(result2).to.equal('world');
+        });
+
+        it('should return the last letter of a string if no second argument is provided', function () {
+            const result = _.last('hello');
+            expect(result).to.equal('o');
+        });
+
+        it('should return the elements up to the nth element of a given array, in an array format', function () {
+            const result1 = _.last([1,2,3,4,5], 1);
+            expect(result1).to.be.an('array');
+            expect(result1).to.eql([5]);
+
+            const result2 = _.last([1,2,3,4,5], 2);
+            expect(result2).to.eql([4, 5]);
+
+            const result3 = _.last(['to', 'be', 'or', 'not', 'to', 'be'], 4);
+            expect(result3).to.eql(['or', 'not', 'to', 'be']);
+        });
+
+        it('should return the letters up to the nth letter of a given string, returning an array', function () {
+            const result1 = _.last('hello', 1);
+            expect(result1).to.be.an('array');
+            expect(result1).to.eql(['o']);
+
+            const result2 = _.last('hello', 3);
+            expect(result2).to.eql(['l', 'l', 'o']);
+        });
+
+         it('should return an empty array if the number is negative or zero', function () {
+            const result1 = _.last([1,2,3,4,5], -1);
+            expect(result1).to.be.an('array');
+            expect(result1).to.eql([]);
+
+            const result2 = _.last([1,2,3,4,5], -3);
+            expect(result2).to.be.an('array');
+            expect(result2).to.eql([]);
+
+            const result3 = _.last([1,2,3,4,5], 0);
+            expect(result3).to.be.an('array');
+            expect(result3).to.eql([]);
+        });
+
+        it('should assume Math.ceil(n) if a decimal input is given for n', function () {
+            const result1 = _.last([1,2,3,4,5], 0.8);
+            expect(result1).to.eql([5]);
+
+            const result2 = _.last([1,2,3,4,5], 2.3);
+            expect(result2).to.eql([3,4,5]);
+
+            const result3 = _.last('hello', 3.3);
+            expect(result3).to.eql(['e','l','l', 'o']);
+
+            
+        });
+
+        it('returns the whole array if n is bigger than the array or string length', function () {
+            const result1 = _.last([1,2,3,4,5], 100);
+            expect(result1).to.eql([1,2,3,4,5]);
+
+            const result2 = _.last('hello', 1000);
+            expect(result2).to.eql(['h', 'e', 'l', 'l', 'o']);
+        });
+
+
+        it('should return an empty array if a string is entered and the number is negative or zero', function () {
+            const result1 = _.last('hippopotamus', -1);
+            expect(result1).to.be.an('array');
+            expect(result1).to.eql([]);
+
+            const result2 = _.last('hippopotamus', -3);
+            expect(result2).to.be.an('array');
+            expect(result2).to.eql([]);
+
+            const result3 = _.last('hippopotamus', 0);
+            expect(result3).to.be.an('array');
+            expect(result3).to.eql([]);
+        });
+
+        it('should return an empty array if the input is an empty array/string and a number', function () {
+            const result1 = _.last([], 1);
+            expect(result1).to.eql([]);
+
+            const result2 = _.last('', 1);
+            expect(result2).to.eql([]);
+        });
+    });
+
 });
 
 
